e96391ab46973d3fbd80ffcb3ad74f9aa30181f8
pramacha <archanakannan1986@gmail.com>
2018-07-31 00:05:02 -0700

    fixed bugs

--- algo.js
+++ algo.js
@@ -301,3 +301,27 @@ function updateInventory(arr1, arr2) {
 // binary representation 10000010001 and so its longest binary gap is of
 // length 5. Given N = 32 the function should return 0, because N has
 // binary representation '100000' and thus no binary gaps.
+
+function solution(N) {
+    let binaryGap = 0;
+    let foundBinaryGap = false;
+    let i=0;
+    while(N > 0){
+        // if N is divisible by 2,this means, it will convert to 0. But since we want it to be enclosed
+        // between 1, we will keep a count on 0s with i and calculate binary gap only when foundBinaryGap
+        // is true (this flag is turned on only when we encounter a number that will return 1 when % 2 is
+        // used.)
+        if (N % 2 === 0) {
+            i += 1;
+        } else {
+           if(i > binaryGap && foundBinaryGap===true) {
+            binaryGap = i;
+          }
+
+          foundBinaryGap = true;
+          i = 0;
+        }
+        N = Math.floor(N/2);
+    }
+    return binaryGap;
+}
