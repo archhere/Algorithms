b94ca5718184acf9747d56588a3020c1246d7b49
pramacha <archanakannan1986@gmail.com>
2018-08-19 12:55:56 -0700

    linked list

---
+++ linked_list.js
@@ -0,0 +1,19 @@
+
+// A very simple implementation of linkedlist
+
+const nodeOne = {
+  data: 123
+};
+
+const nodeTwo = {
+  data: 456
+};
+
+nodeOne.next = nodeTwo;
+
+// These are 2 nodes that arent linked. Now we need to explicitly link
+// them together.
+
+// -------------------------------------------------------------------------
+
+class Node {}
