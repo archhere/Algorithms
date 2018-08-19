51db9b072ece421d1a2f613efe0ed1853304939e
pramacha <archanakannan1986@gmail.com>
2018-08-19 13:31:06 -0700

    implemented linked list with javascript

--- linked_list.js
+++ linked_list.js
@@ -16,4 +16,26 @@ nodeOne.next = nodeTwo;
 
 // -------------------------------------------------------------------------
 
-class Node {}
+class Node {
+  constructor(data,next=null) {
+      this.data = data;
+      this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor(){
+    this.head = null;
+  }
+
+  insertFirst(data){
+    this.head = new Node(data,this.head);
+  }
+}
+
+const list = new LinkedList();
+list.head = new Node(10);
+
+module.exports = {Node,LinkedList};
+
+// -------------------------------------------------------------------------
