3f3411112fb80cfb4cc3295ff4a8101187bc85aa
pramacha <archanakannan1986@gmail.com>
2018-08-19 18:09:25 -0700

    implemented methods like insert, remove etc in linked list

--- linked_list.js
+++ linked_list.js
@@ -31,6 +31,48 @@ class LinkedList {
   insertFirst(data){
     this.head = new Node(data,this.head);
   }
+
+  size(){
+    let count = 0;
+    let currentNode = this.head;
+
+    while (currentNode !== null) {
+      currentNode = currentNode.next;
+      this.count++;
+    }
+    return count;
+  }
+
+  getFirst(){
+    return this.head;
+  }
+
+  // getLast(){
+  //   let currentNode = this.head;
+  //   while (currentNode.next) {
+  //     currentNode = currentNode.next;
+  //   }
+  //   return currentNode;
+  // }
+
+  getLast(){
+    if(!this.head) return null;
+    let currentNode = this.head;
+    while(currentNode){
+      if(!currentNode.next) return currentNode;
+      currentNode = currentNode.next;
+    }
+  }
+
+  clear(){
+    this.head = null;
+  }
+
+  removeFirst(){
+    if (!this.head) return null;
+    let second = this.head.next;
+    this.head = second;
+  }
 }
 
 const list = new LinkedList();
