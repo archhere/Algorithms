4dad2c386712dde65be4c105c3ebeff4c81d8510
pramacha <archanakannan1986@gmail.com>
2018-08-21 22:18:23 -0700

    dfs and bfs

--- tree.js
+++ tree.js
@@ -17,4 +17,34 @@ class Node{
   }
 }
 
-class Tree{}
+class Tree{
+  constructor(){
+    this.root = null;
+  }
+
+// traverseBF and traverseDF will both takein a function as an argument
+  traverseBF(callback){
+    const arr = [this.root];
+    while (arr){
+      const node = arr.shift();
+      // node.children.forEach(child => arr.push(child));
+      arr.push(...node.children);
+      callback(node);
+    }
+  }
+
+  traverseDF(callback){
+    const arr = [this.root];
+    while (arr){
+      const node = arr.shift();
+      arr.unshift(...node.children);
+      callback(node);
+    }
+  }
+
+}
+
+
+// const node = new Node(1);
+// const tree = new Tree();
+// tree.root = node;
